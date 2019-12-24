8f14c259c0d8c9af1980ce9c8be1555e13a88c24
Yashwinie <yashwinie.shivanand@gmail.com>
2019-12-23 20:07:43 -0500

    flask code outline

--- sqlalchemy-challenge/app.py
+++ sqlalchemy-challenge/app.py
@@ -17,15 +17,15 @@ Base = automap_base()
 Base.prepare(engine, reflect=True)
 
 #Save reference to the table
-Measurement = Base.classes.measurement
-Station = Base.classes.station
+Measurements = Base.classes.measurements
+Stations= Base.classes.stations
 
 #Flask setup
 app = Flask(__name__)
 
 #Flask routes
 @app.route("/")
-def welcome():
+def index():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
@@ -35,8 +35,54 @@ def welcome():
     )
 
 @app.route("/api/v1.0/precipitation")
-def precipitation():
-    #Create our session (link) from Python to the database
     session = Session(engine)
+def precipitation():
     
-    """Return a list of precipitation results, including the date and the precipitation"""
+   """Return a list of precipitation results, including the date and the precipitation"""
+   
+    #Query list of precipitation results
+    presults = session.query().all()
+
+    session.close()
+
+    #Convert list of tuples into normal list
+    prec_results = list(np.ravel(presults))
+
+    #view information as json
+    return jsonify (prec_results)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+     session = Session(engine)
+
+    """Return a list of stations from the dataset"""
+    #Query list of station results
+    sresults = session.query().all()
+
+    session.close()
+
+    #Convert list of tuples into normal list
+    station_results = list(np.ravel(sresults))
+
+    #view information as json
+    return jsonify (station_results)
+
+@app.route("tobs")
+def tobs():
+     session = Session(engine)
+
+    """Return a list of of temperature observations for the previous year, including dates and temperature observations"""
+    #Query list of temperature observations
+    tresults = session.query().all()
+
+    session.close()
+
+    #Convert list of tuples into normal list
+    temp_obsv = list(np.ravel(tresults))
+
+    #view information as json
+    return jsonify (temp_obsv)
+
+if __name__ == "__main__":
+    app.run(debug=True)      
