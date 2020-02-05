3fd39c5ff5eac0b1df45f662d83fd880a8911f12
Yashwinie <yashwinie.shivanand@gmail.com>
2020-02-04 23:19:00 -0500

    updates

--- plotly-challenge/Starter_Code/static/js/app.js
+++ plotly-challenge/Starter_Code/static/js/app.js
@@ -4,36 +4,27 @@
 //store samples.json file as variable
 var url = "samples.json"
 
-//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-//use 'sample_values' as values for bar chart, 'otu_ids' as labels for bar chart, and 'otu_labels' as hovertext for the chart.
 
-//make a function to have all teh variables with information we need to build the chart
-function horizontalChart(sample) {
-    var chartData = `/samples/${sample}`;
-    d3.json(chartData).then(function(data) {
-        var x_axis = data.otu_ids.slice(0,10);
-        var y_axis = data.sample_values.slice(0,10);
-        var hover_text = data.otu_labels.slice(0,10);
+
+function unpack (rows, index) {
+    return rows.map(function(row) {
+        return row[index];
     });
 };
 
-var trace1 = {
-    x: x_axis,
-    y: y_axis,
-    text: hover_text,
-    type: "bar"
+//Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+//use 'sample_values' as values for bar chart, 'otu_ids' as labels for bar chart, and 'otu_labels' as hovertext for the chart.
+
+function buildPlots() {
+    d3.json(chartData).then(function(data) {
+        var individual = data.samples.id;
+        var sampleValues = unpack(data.samples.sample_values, 10);
+        var otuIDs = unpack(data.samples.otu_ids, 10);
+        var otuLabels = unpack(data.samples.otu_labels, 10);
+    });
 };
 
-var data =[trace1];
-
-var layout = {
-    title: "Top 10 OTUs found in individual",
-    xaxis: { title: "OTU ID"},
-    yaxis: { title: "Sample Values"}
-};
-
-Plotly.newPlot("plot", data, layout);
-
+buildPlots();
 
 //Create a bubble chart that displays each sample.
 
