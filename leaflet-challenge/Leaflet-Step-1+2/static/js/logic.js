508e9ab9ec67973f60166ac00a222ee9d214356d
Yashwinie <yashwinie.shivanand@gmail.com>
2020-03-21 21:38:07 -0400

    debugging

--- leaflet-challenge/Leaflet-Step-1+2/static/js/logic.js
+++ leaflet-challenge/Leaflet-Step-1+2/static/js/logic.js
@@ -29,6 +29,7 @@ function createFeatures(earthquakeData) {
     else {
       color = "yellow";
     };
+  };
 
     // Give each feature a popup describing the place and time of the earthquake
     function onEachFeature(feature, layer) {
@@ -41,6 +42,7 @@ function createFeatures(earthquakeData) {
       }).bindPopup("<h3>" + feature.properties.place +
         "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
     };
+  };
 
 // Perform a GET request to the platesURL
 d3.json(platesURL, function(platesdata) {
@@ -50,7 +52,7 @@ d3.json(platesURL, function(platesdata) {
       "color": "yellow",
       "weight": 3,
       "opacity": 1
-    };
+    }
   });
 });
   
@@ -67,50 +69,11 @@ d3.json(platesURL, function(platesdata) {
     // Sending our earthquakes layer to the createMap function
   createMap(earthquakes);
   createMap(plates);
-};
 
-function createMap(earthquakes) {
-
-  // Define layers
-
-  var satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox.satellite",
-      accessToken: API_KEY
-    });
-
-  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "mapbox.streets",
-    accessToken: API_KEY
-  });
-
-  // Define a baseMaps object to hold our base layers
-  var baseMaps = {
-    "Satellite Map": satellite,
-    "Street Map": streetmap
-  };
-
-  // Create overlay object to hold our overlay layer
-  var overlayMaps = {
-    "Earthquakes": earthquakes,
-    "Tectonic": plates
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
-  var myMap = L.map("map", {
-    center: [
-      38.85, -19.61
-    ],
-    zoom: 3,
-    layers: [satellite, earthquakes, plates]
-  });
 
   // Create a layer control
   // Pass in our baseMaps and overlayMaps
   // Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
-    }).addTo(myMap);
+    }).addTo(myMap)
